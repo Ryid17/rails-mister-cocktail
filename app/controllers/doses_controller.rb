c1393ba1bea94a7ccf59ee68dc45fd522f5045a6
Ryid Nassif <ryid.nassif@gmail.com>
2020-05-15 11:55:23 +0100

    backend working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,30 +1,24 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:new, :create, :destroy]
-
-  def new
-    @dose = Dose.new
-  end
-
   def create
-    @dose.cocktail = @dose
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.delete
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  def set_dose
-    @dose = dose.find(params[:ingredient_id])
-  end
-
-  def doses_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
